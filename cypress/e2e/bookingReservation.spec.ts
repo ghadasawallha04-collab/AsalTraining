0d9c55ecb29f094b057a9a34e770536353ff6f7c
Ghada Sawalha <ghada.sawallha04@gmail.com>
2026-05-17 09:46:23 +0300

    Adding Reservation flow

--- cypress/e2e/bookingReservation.spec.ts
+++ cypress/e2e/bookingReservation.spec.ts
@@ -1,21 +1,25 @@
 import { BaseActions } from "../pages/base_page/BaseActions";
+import { HotelActions } from "../pages/hotel_page/HotelActions";
 import { SearchActions } from "../pages/search_page/SearchActions";
 import { SearchValidations } from "../pages/search_page/SearchValidation";
-describe('Booking Search Functionality',() =>{
+describe('Booking reservation flow',() =>{
   const base=new BaseActions();
   const search=new SearchActions(); 
   const verify=new SearchValidations();
+  const hotel=new HotelActions();
   //Preconditions
   beforeEach(()=>{
     base.openBookingWebsite();
     base.closeSignInPopup();
   });
-  //Test
- it('Should Perform Search',()=>{
-  const searchOptions={destination:"Rosewood",checkInDate:"2026-06-22",checkOutDate:"2026-06-24",
-    occupancyData:{adults:2,children:[],rooms:1}
+ it('Should search validate hotel and perform booking flow',()=>{
+  const searchOptions={destination:"San Hotel",checkInDate:"2026-06-20",checkOutDate:"2026-06-22",
+    occupancyData:{adults:2,children:[3,15],rooms:1}
   };
+  const RoomData={roomType:"Standard Double Room",guests:3,rateOptions:[]}
   search.performSearch(searchOptions);
   verify.verifyCardSearchDetails(searchOptions);
+  hotel.openFirstProperty();
+  hotel.findRoom(RoomData);
 });
 });

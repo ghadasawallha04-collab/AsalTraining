a9b3de88b77352df0c63449d8acad62b376fc5ab
Ghada Sawalha <ghada.sawallha04@gmail.com>
2026-05-29 09:02:42 +0300

    Fixing closing sign in popup

--- cypress/pages/base_page/BaseActions.ts
+++ cypress/pages/base_page/BaseActions.ts
@@ -2,20 +2,29 @@
 import { CLOSE_SIGN_IN_POPUP_BTN } from "./BaseElements";
 import { getElement } from "../../utils/elementHelpers";
 import { logger } from "../../utils/logger";
+import { DESTINATION_SEARCH_INPUT } from "../search_page/SearchElements";
 export class BaseActions{
     /**
      * Opens the Booking.com website.
-     */    openBookingWebsite(){
+     */
+    openBookingWebsite(){
         logger.step("Opening Booking website");
         cy.visit('https://www.booking.com');
+        cy.get(DESTINATION_SEARCH_INPUT.selector!).should('be.visible');
     }
     /**
      * Closes the sign-in popup if it appears.
      * Ensures the popup is visible before clicking.
      */
     closeSignInPopup(){
-        logger.step("Closing sign-in popup");
-        getElement(CLOSE_SIGN_IN_POPUP_BTN).should('be.visible').click();
-        logger.success("Sign-in popup closed successfully");
-    }
+        cy.get('body').then(($body)=>{
+            if($body.find(CLOSE_SIGN_IN_POPUP_BTN.selector!).length){
+                getElement(CLOSE_SIGN_IN_POPUP_BTN).should('be.visible').click();
+                logger.success("Sign-in popup closed successfully");
+        } 
+        else{
+            logger.info("Sign-in popup was not displayed");
+        }
+    });
+}
 }
